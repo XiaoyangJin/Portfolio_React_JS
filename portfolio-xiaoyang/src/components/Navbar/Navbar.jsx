f0b70adce45571ad4a7d9627af526e980f957c6b
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-03-24 19:33:54 -0400

    Import customized function

--- portfolio-xiaoyang/src/components/Navbar/Navbar.jsx
+++ portfolio-xiaoyang/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import styles from "./Navbar.module.css"
+import React, { useState } from 'react';
+
+import styles from "./Navbar.module.css";
+import { getImageUrl } from "../../utils";
 
 const Navbar = () => {
     return (
