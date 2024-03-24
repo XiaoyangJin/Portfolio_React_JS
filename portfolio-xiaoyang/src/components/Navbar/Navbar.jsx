b5ceb577538c84b77f3919a6952e17545240dbc7
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-03-24 19:59:07 -0400

    Add toggle function to humburger icon

--- portfolio-xiaoyang/src/components/Navbar/Navbar.jsx
+++ portfolio-xiaoyang/src/components/Navbar/Navbar.jsx
@@ -4,12 +4,18 @@ import styles from "./Navbar.module.css";
 import { getImageUrl } from "../../utils";
 
 const Navbar = () => {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <nav className={styles.navbar}>
             <a className={styles.title} href='/'>Porfolio</a>
             <div className={styles.menu}>
-                <img className={styles.menuBtn} src={getImageUrl("nav/menuIcon.png")} alt="a humburger button which can toggle nav bar menu" />
-                <ul className={styles.menuItems}>
+                <img className={styles.menuBtn}
+                    src={menuOpen ? getImageUrl("nav/closeIcon.png") : getImageUrl("nav/menuIcon.png")}
+                    alt="a humburger button which can toggle nav bar menu"
+                    onClick={() => setMenuOpen(!menuOpen)} />
+                <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}>
                     <li><a href='#about'>About</a></li>
                     <li><a href='#experience'>Experience</a></li>
                     <li><a href='#projects'>Projects</a></li>
