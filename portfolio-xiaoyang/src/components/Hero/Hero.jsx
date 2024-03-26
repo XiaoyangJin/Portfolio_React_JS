47f0a521600f5f0b5fe5ca79cef54091a46aaa02
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-03-26 11:05:31 -0400

    Add styles names and remove some intro content

--- portfolio-xiaoyang/src/components/Hero/Hero.jsx
+++ portfolio-xiaoyang/src/components/Hero/Hero.jsx
@@ -1,21 +1,30 @@
 import React from 'react'
+
+import styles from "./Hero.module.css";
 import { getImageUrl } from '../../utils';
 
 const Hero = () => {
     return (
-        <section>
-            <div>
-                <h1>Hi, I'm Xiaoyang</h1>
-                <p>⚡ Primary coding language: Java<br />
+        <section className={styles.container}>
+            <div className={styles.content}>
+                <h1 className={styles.title}>
+                    Hi, I'm Xiaoyang
+                </h1>
+                <p className={styles.description}>⚡ Primary coding language: Java<br />
                     - 💼 I’m looking for: Full-time full-stack engineer role<br />
                     - 🎓 My OPT will start on: July 7th, 2024<br />
                     - 🚀 I’m currently working on: My full-stack project Study Sphere<br />
-                    - 🌱 I’m currently learning: AWS, Angular, Spring...<br />
-                    - 📫 The best way to reach me: jin.xiaoya@northeastern.edu<br />
-                    - 📜 Fun fact: Certifications are my jam – can't stop earning them!</p>
-                <a href="mailto:jin.xiaoya@northeastern.edu">Contact Me</a>
+                    - 🌱 I’m currently learning: AWS, Angular, Spring...
+                </p>
+                <a href="mailto:jin.xiaoya@northeastern.edu"
+                    className={styles.contactBtn}>
+                    Contact Me
+                </a>
             </div>
-            <img src={getImageUrl("hero/heroImagexy.png")} alt="Me at snow montain when snowboarding" />
+            <img src={getImageUrl("hero/heroImagexy.png")}
+                alt="Me at snow montain when snowboarding" className={styles.heroImg} />
+            <div className={styles.topBlur} />
+            <div className={styles.bottomBlur} />
         </section>
     )
 }
