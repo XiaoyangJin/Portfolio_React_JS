bffa037d309c48d908de835a13dcc412216f9015
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-03-26 13:54:14 -0400

    Upload about image

--- portfolio-xiaoyang/src/components/About/About.jsx
+++ portfolio-xiaoyang/src/components/About/About.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { getImageUrl } from '../../utils';
 
 const About = () => {
     return (
-        <div>About</div>
+        <section>
+            <h2>About</h2>
+            <div>
+                <img
+                    src={getImageUrl("about/aboutXY.png")}
+                    alt="Me sitting faced to a laptop using mobile phone to handle messages" />
+            </div>
+        </section>
     )
 }
 
