952a7d38044284e28fb84a673119a8d7c3c118f7
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-03-26 16:47:35 -0400

    Add hover effect and optimize the content to be brief

--- portfolio-xiaoyang/src/components/About/About.jsx
+++ portfolio-xiaoyang/src/components/About/About.jsx
@@ -9,46 +9,37 @@ const About = () => {
             <h2 className={styles.title}>About</h2>
             <div className={styles.content}>
                 <img
-                    src={getImageUrl("about/aboutXY.png")}
+                    src={getImageUrl("about/aboutImage.png")}
                     alt="Me sitting faced to a laptop using mobile phone to handle messages"
                     className={styles.aboutImage} />
+
+                <ul className={styles.aboutItems}>
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+                        <div className={styles.aboutItemText}>
+                            <h3>Website Developer</h3>
+                            <p>At ChickTech, I enhanced website usability and efficiency through
+                                frontend development and Agile methodologies.</p>
+                        </div>
+                    </li>
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+                        <div className={styles.aboutItemText}>
+                            <h3>ESG Data Analyst</h3>
+                            <p>At China Chengxin, I streamlined ESG data analysis and management
+                                using advanced database technologies and web scraping.</p>
+                        </div>
+                    </li>
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl("about/uiIcon.png")} alt="UI icon" />
+                        <div className={styles.aboutItemText}>
+                            <h3>Software Developer</h3>
+                            <p>At Yongyou Software, I improved software reliability and efficiency
+                                with targeted data analysis and process optimization.</p>
+                        </div>
+                    </li>
+                </ul>
             </div>
-            <ul className={styles.aboutItems}>
-                <li className={styles.aboutItem}>
-                    <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
-                    <div className={styles.aboutItemText}>
-                        <h3>Website Developer</h3>
-                        <p>As a Website Developer Intern at ChickTech (May-Sept 2023),
-                            I revamped the registration page and event discovery,
-                            reducing data errors by 25% and increasing user retention by 30%,
-                            while implementing Agile practices and enhancing code quality
-                            through GitHub collaboration.</p>
-                    </div>
-                </li>
-                <li className={styles.aboutItem}>
-                    <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
-                    <div className={styles.aboutItemText}>
-                        <h3>ESG Data Analyst</h3>
-                        <p>As an ESG Data Analyst Intern at China Chengxin International
-                            Credit Rating (Oct 2020-Jan 2021), I improved data retrieval
-                            efficiency from a 30k entry dataset by 30% using PostgreSQL
-                            indexing, streamlined the management of 250+ real estate reports
-                            through Python web scraping and JSON transformation, and enhanced
-                            real-time analysis with a MongoDB database.</p>
-                    </div>
-                </li>
-                <li className={styles.aboutItem}>
-                    <img src={getImageUrl("about/uiIcon.png")} alt="UI icon" />
-                    <div className={styles.aboutItemText}>
-                        <h3>Software Developer</h3>
-                        <p>As a Software Developer Intern at Yongyou Software Co., Ltd
-                            (Aug-Sept 2019), I enhanced data integrity by analyzing 50k
-                            salary points using K-Nearest-Neighbors in Python, improved
-                            UClient software reliability with Java-based unit tests, and
-                            expedited data processes, cutting project timelines from 4 days to 1 day.</p>
-                    </div>
-                </li>
-            </ul>
         </section>
     )
 }
