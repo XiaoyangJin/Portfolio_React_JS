82b1ea581f980df4304c21dfff1749485e30c424
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-03-27 16:15:24 -0400

    Read JSON data and create tech stack

--- portfolio-xiaoyang/src/components/Experience/Experience.jsx
+++ portfolio-xiaoyang/src/components/Experience/Experience.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 
+import skills from "../../data/skills.json";
+import { getImageUrl } from '../../utils';
+
 const Experience = () => {
     return (
-        <div>Experience</div>
+        <section id="experience">
+            <h2>Experience</h2>
+            <div>
+                <div>{
+                    skills.map((skill, id) => {
+                        return <div key={id}>
+                            <div>
+                                <img src={getImageUrl(skill.imageSrc)} alt="Skills icon" />
+                            </div>
+                            <p>{skill.title}</p>
+                        </div>
+                    })
+                }</div>
+                <ul></ul>
+            </div>
+        </section>
     )
 }
 
