797b946e7316f896c061febb2c46fb75cfaecefd
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-03-27 22:26:18 -0400

    Add other project info from json

--- portfolio-xiaoyang/src/components/Projects/Projects.jsx
+++ portfolio-xiaoyang/src/components/Projects/Projects.jsx
@@ -11,6 +11,19 @@ const Projects = () => {
                 projects.map((project, id) => {
                     return <div key={id}>
                         <img src={getImageUrl(project.imageSrc)} alt={`Image of ${project.title}`} />
+                        <h3>{project.title}</h3>
+                        <p>{project.description}</p>
+                        <ul>
+                            {
+                                project.skills.map((skill, id) => {
+                                    return <li key={id}>{skill}</li>;
+                                })
+                            }
+                        </ul>
+                        <div>
+                            <a href={project.demo}>Demo</a>
+                            <a href={project.source}>Source</a>
+                        </div>
                     </div>
                 })
             }
