66f388cde46552c8c3fa91fd13cf8b139de4b707
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-03-27 22:20:35 -0400

    Read projects image from json file

--- portfolio-xiaoyang/src/components/Projects/Projects.jsx
+++ portfolio-xiaoyang/src/components/Projects/Projects.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
+import projects from "../../data/projects.json";
+import { getImageUrl } from '../../utils';
+
 const Projects = () => {
-    return;
+    return <section>
+        <h2>Projects</h2>
+        <div>
+            {
+                projects.map((project, id) => {
+                    return <div key={id}>
+                        <img src={getImageUrl(project.imageSrc)} alt={`Image of ${project.title}`} />
+                    </div>
+                })
+            }
+        </div>
+    </section>;
 }
 
 export default Projects;
