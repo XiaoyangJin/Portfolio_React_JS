adf7b6c3fea5c5b6e80b9e00e19fb1d3683b0d70
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-03-27 23:18:37 -0400

    Add basic content in contact part

--- portfolio-xiaoyang/src/components/Contact/Contact.jsx
+++ portfolio-xiaoyang/src/components/Contact/Contact.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import { getImageUrl } from '../../utils';
 
 const Contact = () => {
     return (
-        <div>Contact</div>
+        <footer id="contact">
+            <div>
+                <h2>Contact</h2>
+                <p>Feel free to reach out!</p>
+            </div>
+            <ul>
+                <li>
+                    <img src={getImageUrl("contact/emailIcon.png")} alt="Email Icon" />
+                    <a href="mailto:jin.xiaoya@northeastern.edu">jin.xiaoya@northeastern.edu</a>
+                </li>
+                <li>
+                    <img src={getImageUrl("contact/linkedinIcon.png")} alt="LinkedIn Icon" />
+                    <a href="https://www.linkedin.com/in/xiaoyangjin4/">linkedin.com/xiaoyangjin</a>
+                </li>
+                <li>
+                    <img src={getImageUrl("contact/githubIcon.png")} alt="Github Icon" />
+                    <a href="https://github.com/XiaoyangJin">github.com/xiaoyangjin</a>
+                </li>
+            </ul>
+        </footer>
     )
 }
 
