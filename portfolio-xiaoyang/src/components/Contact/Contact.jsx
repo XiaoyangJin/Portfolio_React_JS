5eeadd7547595bf7d4eb6532cf24b556eafd127f
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-03-27 23:22:49 -0400

    Add styles for contact

--- portfolio-xiaoyang/src/components/Contact/Contact.jsx
+++ portfolio-xiaoyang/src/components/Contact/Contact.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
+
 import { getImageUrl } from '../../utils';
+import styles from "./Contact.module.css";
 
 const Contact = () => {
     return (
-        <footer id="contact">
-            <div>
+        <footer id="contact" className={styles.container}>
+            <div className={styles.text}>
                 <h2>Contact</h2>
                 <p>Feel free to reach out!</p>
             </div>
-            <ul>
-                <li>
+            <ul className={styles.links}>
+                <li className={styles.link}>
                     <img src={getImageUrl("contact/emailIcon.png")} alt="Email Icon" />
                     <a href="mailto:jin.xiaoya@northeastern.edu">jin.xiaoya@northeastern.edu</a>
                 </li>
-                <li>
+                <li className={styles.link}>
                     <img src={getImageUrl("contact/linkedinIcon.png")} alt="LinkedIn Icon" />
                     <a href="https://www.linkedin.com/in/xiaoyangjin4/">linkedin.com/xiaoyangjin</a>
                 </li>
-                <li>
+                <li className={styles.link}>
                     <img src={getImageUrl("contact/githubIcon.png")} alt="Github Icon" />
                     <a href="https://github.com/XiaoyangJin">github.com/xiaoyangjin</a>
                 </li>
