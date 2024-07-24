0823fd8ff053c7d81eb0f3def3650f08198865bc
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-07-24 12:10:12 -0400

    Remove LinkedIn

--- portfolio-xiaoyang/src/components/Contact/Contact.jsx
+++ portfolio-xiaoyang/src/components/Contact/Contact.jsx
@@ -15,10 +15,10 @@ const Contact = () => {
                     <img src={getImageUrl("contact/emailIcon.png")} alt="Email Icon" />
                     <a href="mailto:jin.xiaoya@northeastern.edu">jin.xiaoya@northeastern.edu</a>
                 </li>
-                <li className={styles.link}>
+                {/* <li className={styles.link}>
                     <img src={getImageUrl("contact/linkedinIcon.png")} alt="LinkedIn Icon" />
                     <a href="https://www.linkedin.com/in/xiaoyangjin4/">linkedin.com/xiaoyangjin</a>
-                </li>
+                </li> */}
                 <li className={styles.link}>
                     <img src={getImageUrl("contact/githubIcon.png")} alt="Github Icon" />
                     <a href="https://github.com/XiaoyangJin">github.com/xiaoyangjin</a>
